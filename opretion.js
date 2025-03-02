c2a56832938b55d699644e16960243060cfbf847
SRvasoya5 <srvasoya5@gmail.com>
2025-03-02 11:38:57 +0530

    first commit

---
+++ opretion.js
@@ -0,0 +1,26 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+
+
+// let srt1 = "hello"
+// let srt2 = "_SR vasoya"
+// let srt3= srt1 + srt2
+// console.log(srt3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2"+5+6);
+// console.log("1"+2+2);
+// console.log("1"+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gemeCounter = 100;
+gemeCounter++;
+console.log(gemeCounter);
+
+
+
